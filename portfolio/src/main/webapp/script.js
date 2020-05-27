d3cf74ebc0e9f948ff7980fadfc0faf4ad20b8e0
Mafe Ponce <mafe.ponce@gmail.com>
2020-05-27 13:21:00 -0600

    Replace <br> tags, add more content, style further
    Commit gets rid of <br> tags and uses <p> tags instad.
    More site content is added in the HTML file, which has been divided into sections styled in CSS.
    Small changes made to the button message in JS.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,16 +13,16 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Adds a random Arlo message to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function addRandomArloMessage() {
+  const arloMessages =
+      ['Woof!', 'Woof, Woof!', 'Squirrel?！', 'Treats?!', 'Park?!', 'Zzzzz'];
 
   // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const arloMessage = arloMessages[Math.floor(Math.random() * arloMessages.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const arloTalksContainer = document.getElementById('arlo-talks-container');
+  arloTalksContainer.innerText = arloMessage;
 }
