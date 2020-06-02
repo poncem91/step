be774e8b6e6d1c0ab55860369f9d45314347d035
Mariafe Ponce <mafe.ponce@gmail.com>
2020-06-02 14:12:46 -0600

    Revert "add comment form and comment history"

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -117,14 +117,15 @@ document.addEventListener('keydown', function(event) {
     }
 });
 
-/** Fetches Comments History */
-function getComments() {
-  fetch('/commentshistory').then(response => response.json()).then((comments) => {
-    const commentsHistory = document.getElementById('comments-history');
-    comments.forEach(message => {
+/** Welcome Comments */
+function sayHello() {
+  fetch('/data').then(response => response.json()).then((helloMessage) => {
+    const helloElement = document.getElementById('hello-container');
+    helloElement.innerHTML = '';
+    Object.values(helloMessage).forEach(message => {
         var listNode = document.createElement('li');
         listNode.innerText = message;
-        commentsHistory.appendChild(listNode);
+        helloElement.appendChild(listNode);
     })
   });
 }
