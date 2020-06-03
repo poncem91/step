c32744b8e5987d9ba996fb22ee1dd29355203a7c
Mafe Ponce <mafe.ponce@gmail.com>
2020-06-03 13:34:20 +0000

    fix comment typos

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -21,7 +21,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns Hello Message */
+/** Servlet that allows users to add comments and returns comment history */
 @WebServlet("/comments")
 public class DataServlet extends HttpServlet {
     
