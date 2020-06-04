388ece1000c8fb734949a9a4d798a3d1ef418079
Mafe Ponce <mafe.ponce@gmail.com>
2020-06-04 13:36:52 +0000

    limits datastore results to number of maxComments
    Commit makes use of datastore's FetchOptions to set the limit for the query to maxComments. This limits the number of returned comments.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,8 +19,10 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.FetchOptions;
 import com.google.gson.Gson;
 import java.util.ArrayList;
+import java.util.List;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -38,7 +40,6 @@ public class DataServlet extends HttpServlet {
 
     Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    PreparedQuery results = datastore.prepare(query);
 
     String maxCommentsString = request.getParameter("numcomments");
 
@@ -49,15 +50,13 @@ public class DataServlet extends HttpServlet {
         System.err.println("Could not convert to int: " +  maxCommentsString);
     }
 
+    List<Entity> results = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(maxComments));
+
     ArrayList<String> comments = new ArrayList<>();
 
-    int currentCount = 0;
-
-    for (Entity entity : results.asIterable()) {
-      if (currentCount >= maxComments) break;
+    for (Entity entity : results) {
       String message = (String) entity.getProperty("message");
       comments.add(message);
-      currentCount++;
     }
 
     Gson gson = new Gson();
